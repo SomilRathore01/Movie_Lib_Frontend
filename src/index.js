cd8a5c8bf47ad381e56cb7d29f68c413fe986e0c
SomilRathore01 <112357002+SomilRathore01@users.noreply.github.com>
2024-09-09 04:08:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-axios.defaults.baseURL = `http://localhost:3000`
+axios.defaults.baseURL = `https://movie-lib-backend.onrender.com`
 root.render(
   <React.StrictMode>
     <BrowserRouter>
